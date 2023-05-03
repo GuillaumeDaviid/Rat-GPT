15391868fca58fc0bb293d7cfe57ea71b2df692b
Guillaume David <guillaumedaavidpro@gmail.com>
2023-05-03 10:26:51 +0200

    config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-const { Configuration, OpenAIApi } = require("openai");
 import Chat from './components/Chat.jsx';
 
-function App() {
+
+function App() {/*
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  console.log(process.env.OPENAI_API_KEY) 
+
+  const openai = new OpenAIApi(configuration);
+
+  const generate = async () => {
+    const res = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      temperature: 0,
+      max_tokens: 7,
+    });
+
+    console.log(res.data.data[0])
+
+    return <div></div>
+  }*/
+
   return (
     <div className="App">
       <Chat />
+
+      <button>generate</button>
     </div>
   );
 }
