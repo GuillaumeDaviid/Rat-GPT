53ae156ac3801669d81f703a625f897edee1cfb2
Guillaume David <guillaumedaavidpro@gmail.com>
2023-05-04 11:35:46 +0200

    integration de l'api au composant Chat

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import './App.css';
-import { Configuration, OpenAIApi } from 'openai';
 import Chat from './components/Chat.jsx';
 
 
 function App() {
-  const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-  });
-
-  console.log(process.env.REACT_APP_OPENAI_API_KEY)
-
-  const openai = new OpenAIApi(configuration);
-
-  const generate = async () => {
-    const res = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Say this is a test",
-      temperature: 0,
-      max_tokens: 7,
-    });
-
-    console.log(res.data.choices[0].text)
-
-    return <div></div>
-  }
-
   return (
     <div className="App">
       <Chat />
-
-      <button onClick={generate}>generate</button>
     </div>
   );
 }
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,18 +1,40 @@
 import { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
 import './Chat.css'
 import { msgBot } from './Messages.jsx';
 
 function Chat() {
-
     const [msg, setMsg] = useState('');
     const [countMsg, setCountMsg]= useState(0);
     const [newMsg, setNewMsg]= useState([]);
+    const [msgGpt, setMsgGpt] = useState([]);
+
+    const configuration = new Configuration({
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      });
+
+    const openai = new OpenAIApi(configuration);
+
+    const generate = async () => {
+       const res = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: newMsg.slice(-1),
+        temperature: 0,
+        max_tokens: 7,
+    });
+
+    console.log(res.data.choices[0].text)
+    setMsgGpt(msgGpt.concat(res.data.choices[0].text));
+
+    return <div></div>
+  }
 
     function handleClick(e){
         e.preventDefault();
         setCountMsg(countMsg + 1);
         setNewMsg(newMsg.concat(msg))
         setMsg('');
+        generate();
     }
 
     function handleChange(e){
@@ -21,7 +43,7 @@ function Chat() {
 
     const listMsg = countMsg > 0 ?
     (newMsg.map((newMsg) => <div className='chat_bot_content-msg'><h2 className='chat_bot-msg'>Moi :</h2><p className='chat_bot-msg' key={newMsg.key}>{newMsg}</p>
-    <h2 className='chat_bot-msg'>Bot :</h2> {msgBot(newMsg)}</div>))
+    <h2 className='chat_bot-msg'>Bot :</h2> {msgGpt.slice(-1)}</div>))
  : (<p></p>)
 
     return(
