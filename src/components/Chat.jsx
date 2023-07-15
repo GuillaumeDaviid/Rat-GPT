6ba5e24e04dcfcb2a394995cda2c3e11ca81f249
Guillaume David <guillaumedaavidpro@gmail.com>
2023-07-15 19:47:24 +0200

    add api externe

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,31 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import Modal from './Modal'
 import './Chat.css'
 
 function Chat() {
     const [msg, setMsg] = useState('');
     const [countMsg, setCountMsg]= useState(0);
     const [newMsg, setNewMsg]= useState({'me': [], 'bot': []})
+    const [api, setApi] = useState();
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('api')){
+            console.log(localStorage.getItem('api'))
+            setApi(localStorage.getItem('api'));
+        }
+    })
+
+    console.log(api)
 
 
     const configuration = new Configuration({
-        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+        apiKey: api,
       });
 
     const openai = new OpenAIApi(configuration);
 
     const generate = async () => {
-       const res = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: msg,
-        temperature: 0,
-        max_tokens: 7,
-    });
+        try {
+          const res = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: msg,
+            temperature: 0,
+            max_tokens: 7,
+          });
 
-    setNewMsg({'me' : newMsg.me.concat(msg), 'bot': newMsg.bot.concat(res.data.choices[0].text)})
+          setNewMsg({'me' : newMsg.me.concat(msg), 'bot': newMsg.bot.concat(res.data.choices[0].text)})
 
-    return <div></div>
-  }
+          setError(false)
+        } catch (err) {
+          // Gérer les erreurs
+          console.error("Erreur lors de la génération :", err);
+          setError(err);
+        }
+      
+        return <div></div>; // Rendu vide (peut-être remplacé par le rendu souhaité)
+      };
 
     function handleClick(e){
         e.preventDefault();
@@ -48,12 +68,15 @@ function Chat() {
     return(
         <div>
             <h1>Chat Bot with GPT</h1>
+            <div><Modal/></div>
             <div className="chat_bot" id="chat_bot">
                 <div className='chat_bot_content-msg' id='chat_bot_content-msg'>
                 <h2 className='chat_bot-msg'>Bot :</h2>
                 <p className='chat_bot-msg'>Bonjour, Je suis un bot codé en React.js et utilisant GPT3 d'Open AI</p>
                 </div>
-                {listMsg}
+                {
+                    error ? <p>Veuillez entrer une clé API valide !</p> : listMsg
+                }
                 
                 
             </div>
---
+++ src/components/Modal.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import './Modal.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+        <button className="modal-close" onClick={onClose}>
+          Fermer
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ParentComponent = () => {
+    const [message, setMessage] = useState("");
+    const [api, setApi]= useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        const storedApi = localStorage.getItem('api');
+        if (storedApi) {
+          setApi(storedApi);
+        } else {
+          setIsModalOpen(true);
+        }
+      }, []);
+
+    useEffect(() => {
+        localStorage.setItem('api', api);
+      }, [api]);
+
+    
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleClick = () => {
+    setApi(message);
+    setIsModalOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Entrez votre clé API</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Entrez votre clé API !</h2>
+        <input type="text"
+               id="api"
+               name="api"
+               onChange={handleChange}
+               value={message} />
+        <button onClick={handleClick}>Envoyer</button>
+        <p>Votre clé ne sera stockée qu'en local*</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default ParentComponent;
